77c2e03b0cabd5c5f8c681c95ce365798dc5bbd9
dongyoun <doong1122@naver.com>
2026-05-22 21:04:58 +0900

    Add: Final version of logger module (Daily log rotation & Auto-user detection)

--- include/logger.h
+++ include/logger.h
@@ -1,21 +1,21 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
-// 팀 공통 약속: 패스워드 에러는 -1, 성공은 1
+// 팀 공통 약속
 #define PASSWORD_ERROR -1
 #define SUCCESS 1
 
-// 팀 공통 약속: 모든 파일명, 디렉토리명, 패스워드 길이는 1024로 통일
+// 팀 공통 약속: 길이 규격 1024
 #define MAX_PATH_LEN 1024
 #define MAX_PW_LEN   1024
 #define MAX_USER_LEN 1024
 
 /*
  * 로그를 기록하는 함수
- * user: 사용자 이름 (최대 1024)
+ * [변경] 매개변수에서 user가 제외되었습니다. 함수 내부에서 자동으로 유저명을 가져옵니다.
  * filename: 접근하려는 파일명 (최대 1024)
  * result: SUCCESS(1) 또는 PASSWORD_ERROR(-1)
  */
-void write_access_log(const char *user, const char *filename, int result);
+void write_access_log(const char *filename, int result);
 
 #endif
